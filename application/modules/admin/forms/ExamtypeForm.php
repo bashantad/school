98611a08d9ceef2f5b5188882128e0a73a4f4942
IO <io@ubuntu.ubuntu-domain>
2012-10-05 20:32:39 +0545

    examtype added

--- application/modules/admin/forms/ExamtypeForm.php
+++ application/modules/admin/forms/ExamtypeForm.php
@@ -1,15 +1,24 @@
 <?php
 
-class Admin_Form_SubjectForm extends Zend_Form {
+class Admin_Form_ExamtypeForm extends Zend_Form {
 
     public function init() {
 
         /* fetching grade options from class.ini file */
         $config = new Zend_Config_Ini(BASE_PATH . DIRECTORY_SEPARATOR . "configs" . DIRECTORY_SEPARATOR . "class.ini", "production");
-//        var_dump($config->latitude->home);exit;
         $gradeOptions = $config->grade->toArray();
+        
+        $examtypeID = new Zend_Form_Element_Hidden("examtype_id");
 
-        $subjectID = new Zend_Form_Element_Hidden("subject_id");
+        $name = new Zend_Form_Element_Text("name");
+        $name->setLabel("Name")
+                ->setAttribs(array('size' => 30, 'class' => 'form-text'))
+                ->setRequired(true);
+
+        $full_marks = new Zend_Form_Element_Text("full_marks");
+        $full_marks->setLabel("Full Marks")
+                ->setAttribs(array('size' => 30, 'class' => 'form-text'))
+                ->setRequired(true);
 
         $grade = new Zend_Form_Element_Select("grade");
         $grade->setLabel("Grade")
@@ -17,8 +26,8 @@ class Admin_Form_SubjectForm extends Zend_Form {
                 ->addMultiOptions($gradeOptions)
                 ->setRequired(true);
 
-        $name = new Zend_Form_Element_Text("name");
-        $name->setLabel("Name")
+        $pass_marks = new Zend_Form_Element_Text("pass_marks");
+        $pass_marks->setLabel("Pass Marks")
                 ->setAttribs(array('size' => 30, 'class' => 'form-text'))
                 ->setRequired(true);
 
@@ -26,9 +35,11 @@ class Admin_Form_SubjectForm extends Zend_Form {
         $submit->setLabel("Submit");
 
         $this->addElements(array(
-            $subjectID,
-            $grade,
+            $examtypeID,
             $name,
+            $grade,
+            $full_marks,
+            $pass_marks,
             $submit));
     }
 
